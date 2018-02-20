0ee9fc9c519d3e19f9d02b7db92a338b20ea065b
fekete20 <fekete20@iit442.iit.uni-miskolc.hu>
2018-02-20 17:32:42 +0100

    Bumm projekt.

---
+++ bumm/src/bumm/bumm.java
@@ -0,0 +1,14 @@
+package bumm;
+
+public class bumm {
+	public static void main(String[] args) {
+		
+		for(int i=10; i>0; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Bumm!");
+
+	}
+	
+	
+}
